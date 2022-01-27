d3230dcf71cff7e75f08b05dfca64af4e9516689
Junliang Yan <junyan@redhat.com>
2022-01-27 15:53:39 -0500

    initial commit

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# create user
+useradd -mG wheel johnyan
+echo -e "johnyan\njohnyan" | passwd johnyan
+
+# install packages
+pacman -Syu --needed --noconfim sudo \
+                              base-devel \
+                              zsh \
+                              git \
+                              lightdm \
+                              xorg \
+                              xorg-server \
+                              qtile \
+                              lxappearance \
+                              nitrogen \
+                              xfce4-terminal \
+                              picom \
+                              archlinux-wallpaper \
+                              lightdm-gtk-greeter \
+                              openssh \
+                              vim
+
+# enable sudo for user
+echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+# change default shell to zsh
+chsh -s /bin/zsh johnyan
+
+# configure display
+sed -i 's/#display-setup-script=/display-setup-script=xrandr --output Virtual-1 --mode 1920x1080/' /etc/lightdm/lightdm.conf
+
+# configure auto login
+sed -i 's/#autologin-guess=false/autologin-guess=false/' /etc/lightdm/lightdm.conf
+sed -i 's/#autologin-user=/autologin-user=johnyan/' /etc/lightdm/lightdm.conf
+sed -i 's/#autologin-user-timeout=0/autologin-user-timeout=0/' /etc/lightdm/lightdm.conf
+groupadd -r autologin
+gpasswd -a johnyan autologin
+
+# enable lightdm
+systemctl enable lightdm.service
+
+# enable ssh server
+systemctl enable sshd.service
+
+# user setup
+sudo -u johnyan /bin/bash user-setup.sh
---
+++ user-setup.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+# install yay
+cd ~
+git clone https://aur.archlinux.org/yay-bin
+cd yay-bin
+makepkg -si --noconfim
+
+cd ~
+
+# install fonts
+yay -S ttf-meslo-nerd-font-powerlevel10k
+
+# configure zsh
+git clone --recursive https://github.com/john-yan/ohmyzsh.git .oh-my-zsh
+cp .oh-my-zsh/zshrc ~/.zshrc
+cp .oh-my-zsh/p10k.zsh ~/.p10k.zsh
+
+echo "nitrogen --restore" > .xprofile
+echo "picom -f &"  >> .xprofile
+
+# create config directory
+mkdir -p .config/{qtile,nitrogen,xfce4}
+mkdir -p .config/xfce4/terminal/
+
+# configure picom
+cp /etc/xdg/picom.conf .config/picom.conf
+sed -i 's/vsync = true/vsync = false/' .config/picom.conf
+
+# configure nitrogen
+cat > .config/nitrogen/bg-saved.cfg << "EOF"
+[xin_-1]
+file=/usr/share/backgrounds/archlinux/landscape.jpg
+mode=4
+bgcolor=#000000
+EOF
+
+cat > .config/nitrogen/nitrogen.cfg << "EOF"
+[geometry]
+posx=960
+posy=0
+sizex=952
+sizey=1048
+
+[nitrogen]
+view=icon
+recurse=true
+sort=alpha
+icon_caps=false
+dirs=/usr/share/backgrounds/archlinux;
+EOF
+
+# configure terminal
+cat > .config/xfce4/ternimal/terminalrc << "EOF"
+[Configuration]
+MiscAlwaysShowTabs=FALSE
+MiscBell=FALSE
+MiscBellUrgent=FALSE
+MiscBordersDefault=TRUE
+MiscCursorBlinks=FALSE
+MiscCursorShape=TERMINAL_CURSOR_SHAPE_BLOCK
+MiscDefaultGeometry=80x24
+MiscInheritGeometry=FALSE
+MiscMenubarDefault=FALSE
+MiscMouseAutohide=FALSE
+MiscMouseWheelZoom=TRUE
+MiscToolbarDefault=FALSE
+MiscConfirmClose=TRUE
+MiscCycleTabs=TRUE
+MiscTabCloseButtons=TRUE
+MiscTabCloseMiddleClick=TRUE
+MiscTabPosition=GTK_POS_TOP
+MiscHighlightUrls=TRUE
+MiscMiddleClickOpensUri=FALSE
+MiscCopyOnSelect=FALSE
+MiscShowRelaunchDialog=TRUE
+MiscRewrapOnResize=TRUE
+MiscUseShiftArrowsToScroll=FALSE
+MiscSlimTabs=FALSE
+MiscNewTabAdjacent=FALSE
+MiscSearchDialogOpacity=100
+MiscShowUnsafePasteDialog=TRUE
+ScrollingBar=TERMINAL_SCROLLBAR_NONE
+BackgroundMode=TERMINAL_BACKGROUND_TRANSPARENT
+BackgroundDarkness=0.800000
+FontName=MesloLGS NF 12
+FontAllowBold=FALSE
+Encoding=UTF-8
+EOF
+
+
